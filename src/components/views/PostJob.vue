4718ccfb2b60d4851413d12fd0980b854baa1e46
Vinicius Mazza <viniciusmazza2552@gmail.com>
2021-12-17 09:32:12 -0300

    feat: add template in PostJob component

--- src/components/views/PostJob.vue
+++ src/components/views/PostJob.vue
@@ -1,6 +1,52 @@
 <template>
-  <div>
-    <p>Publicar Vaga</p>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais</h4>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Título da Vaga</label>
+        <input type="text" class="form-control">
+        <div class="form-text">Por exemplo: Programador JavaScript Vuejs.</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Descrição</label>
+        <textarea type="text" class="form-control"></textarea>
+        <div class="form-text">Informe os detalhes da vaga.</div>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Salário</label>
+        <input type="number" class="form-control">
+        <div class="form-text">Informe o salário</div>
+      </div>
+      <div class="col">
+        <label class="form-label">Modalidade</label>
+        <select class="form-control">
+          <option value="home-office">Home Office</option>
+          <option value="presencial">Presencial</option>
+        </select>
+        <div class="form-text">Informe onde as atividades serão realizadas.</div>
+      </div>
+      <div class="col">
+        <label class="form-label">Tipo de contratação</label>
+        <select class="form-control">
+          <option value="pj">PJ</option>
+          <option value="clt">CLT</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </div>
+    </div>
   </div>
 </template>
 
