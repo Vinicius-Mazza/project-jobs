905c6c1987e60e3aaaa06b14f35dcd46c9b25952
Vinicius Mazza <viniciusmazza2552@gmail.com>
2021-12-20 08:48:42 -0300

    feat: storing data in local storage

--- src/components/views/PostJob.vue
+++ src/components/views/PostJob.vue
@@ -8,26 +8,27 @@
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Título da Vaga</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="title">
         <div class="form-text">Por exemplo: Programador JavaScript Vuejs.</div>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Descrição</label>
-        <textarea type="text" class="form-control"></textarea>
+        <textarea type="text" class="form-control" v-model="description"></textarea>
         <div class="form-text">Informe os detalhes da vaga.</div>
       </div>
     </div>
     <div class="row mt-3">
       <div class="col">
         <label class="form-label">Salário</label>
-        <input type="number" class="form-control">
+        <input type="number" class="form-control" v-model="salary">
         <div class="form-text">Informe o salário</div>
       </div>
       <div class="col">
         <label class="form-label">Modalidade</label>
-        <select class="form-control">
+        <select class="form-control" v-model="model">
+          <option value="" disabled>--Selecione</option>
           <option value="home-office">Home Office</option>
           <option value="presencial">Presencial</option>
         </select>
@@ -35,16 +36,17 @@
       </div>
       <div class="col">
         <label class="form-label">Tipo de contratação</label>
-        <select class="form-control">
+        <select class="form-control" v-model="type">
+          <option value="" disabled>--Selecione</option>
           <option value="pj">PJ</option>
           <option value="clt">CLT</option>
         </select>
         <div class="form-text">Informe o tipo de contratação.</div>
       </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
       <div class="col">
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
+        <button type="submit" class="btn btn-primary" @click="saveJob()">Cadastrar</button>
       </div>
     </div>
   </div>
@@ -52,7 +54,32 @@
 
 <script>
   export default {
-    name: 'PostJob'
+    name: 'PostJob',
+    data: () => ({
+      title: '',
+      description: '',
+      salary: 0,
+      model: '',
+      type: '',
+      publish: ''
+    }),
+    methods: {
+      saveJob() {
+        let jobs = JSON.parse(localStorage.getItem('jobs'))
+        if(!jobs) jobs = []
+
+        jobs.push({
+          title: this.title,
+          description: this.description,
+          salary: this.salary,
+          model: this.model,
+          type: this.type,
+          publish: this.publish
+        })
+
+        localStorage.setItem('jobs', JSON.stringify(jobs))
+      }
+    }
   }
 </script>
 <style></style>
